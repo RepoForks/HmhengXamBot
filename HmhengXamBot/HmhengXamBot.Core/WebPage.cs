a2360c01614b936be84af4b59bbab7bc5be4fba9
Marvin Heng <Marvin.Heng@avire-global.com>
2017-11-11 23:00:51 +0800

    Add project files.

---
+++ HmhengXamBot/HmhengXamBot.Core/WebPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace HmhengXamBot.Core
+{
+	public class WebPage : ContentPage
+	{
+		public WebPage ()
+		{
+			
+            var browser = new WebView();
+            browser.Source = "https://webchat.botframework.com/embed/<BOT_NAME_HERE>?s=<BOT_SECRET_KEY_HERE>";
+            this.Content = browser;
+
+        }
+    }
+}
